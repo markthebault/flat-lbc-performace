135b056d2a3c2b6492b4e40516dc9fe58e9e03c0
Mark <mark.thebault@gmail.com>
2017-06-17 18:22:34 +0200

    Add item detail

--- scrap.py
+++ scrap.py
@@ -7,27 +7,53 @@ import json
 lb_clean_price = lambda x : re.sub("[^0-9]", "", x)
 lb_clean_text = lambda x: x.strip().encode("ascii", "ignore")
 
+items_to_save = ['Surface', 'GES', 'Pices', 'Classe nergie', 'Type de bien']
+def extract_item_detail(url):
+    print ("-----"+url)
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    section = tree.xpath('//section[@class="properties lineNegative"]')[0]
 
+    detail = section.xpath('div[@class="line properties_description"]/p[@itemprop="description"]/text()')
+    detail = (lb_clean_text(detail[0]) if detail else "")
+    properties = section.xpath('div/h2[@class="clearfix"]')
+    dic = {}
+    for prop in properties:
+        cat = prop.xpath('span[@class="property"]/text()')
+        val = prop.xpath('span[@class="value"]/text()')
 
+        cat = (lb_clean_text(cat[0]) if cat else "")
+        val = (lb_clean_text(val[0]) if val else "")
+        if(cat in items_to_save):
+            dic[cat] = val
+
+    dic['description'] = detail
+    return dic
 
 def extract_item_from_url(url, current_deep):
+    print url
     items = []
     page = requests.get(url)
     tree = html.fromstring(page.content)
     sections = tree.xpath('//a[@class="list_item clearfix trackable"]')
     for section in sections:
-        href=section.xpath('@href')
+        href= '%s%s' % ("http:",section.xpath('@href')[0])
         price = map(lb_clean_price, section.xpath('section/h3[@class="item_price"]/text()'))
         name = map(lb_clean_text, section.xpath('section/h2[@class="item_title"]/text()'))
         city = map(lb_clean_text, section.xpath('section/p/meta[@itemprop="address"]/@content'))
+        date = section.xpath('section/aside/p[@class="item_supp"]/@content')
         depatement = (city[1] if len(city) >1 else '')
-        if price and name and city and href:
+        item_detail = extract_item_detail(href)
+
+        if price and name and city and href and date:
             item = {
             'price':price[0],
             'name':name[0],
             'city':city[0],
+            'date':date[0],
             'depatement':depatement,
-            'href':'%s%s' % ("http:",href[0])
+            'href':href,
+            'detail':item_detail
             }
             items.append(item)
     next_link = tree.xpath('//a[@id="next"]/@href')
@@ -35,7 +61,6 @@ def extract_item_from_url(url, current_deep):
     #Look for the next page
     if next_link:
         next_url = '%s%s' % ("http:",next_link[0])
-        print next_url
         if current_deep > 1:
             items = items + extract_item_from_url(next_url, current_deep - 1)
     return items
